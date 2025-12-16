6082e43a68561ade95e2ba4a66da03eca81994a4
ashtonb-sudo <ashtonb@monnit.com>
2025-12-16 10:40:36 -0700

    added in ci/cd yaml files.

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -4,9 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      // any request starting with /api goes to the MVC backend
+      // any request starting with /api goes to the .net API
       "/api": {
         target: "https://localhost:7212",
         changeOrigin: true,
